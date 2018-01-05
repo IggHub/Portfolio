038ff3ad14467b699828cc41c53a0b347584759f
Iggy <igoririanto@rocketmail.com>
2018-01-05 15:37:55 -0800

    add conditionals on thumb_image and add create new actions for experiences

--- app/controllers/experiences_controller.rb
+++ app/controllers/experiences_controller.rb
@@ -2,4 +2,20 @@ class ExperiencesController < ApplicationController
   def index
     @experiences = Experience.all
   end
+
+  def new
+    @experience = Experience.new
+  end
+
+  def create
+    @experience = Experience.new(params.require(:experience).permit(:title, :subtitle, :body))
+
+    respond_to do |format|
+      if @experience.save
+        format.html { redirect_to experiences_path, notice: 'Blog was successfully created.' }
+      else
+        format.html { render :new }
+      end
+    end
+  end
 end
