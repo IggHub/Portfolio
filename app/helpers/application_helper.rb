0e4f979bddbc6ec89ba400db52a7b4af8fa1eee9
Iggy <igoririanto@rocketmail.com>
2018-01-17 13:48:33 -0800

    add rail scontent_tag with helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,8 +4,15 @@ module ApplicationHelper
        link_to "Edit", edit_user_registration_path
        link_to "Logout", destroy_user_session_path, method: :delete
      else
-       (link_to "Login", new_user_session_path) + "\n" + 
+       (link_to "Login", new_user_session_path) + "\n" +
        (link_to "Register", new_user_registration_path)
      end
+  end
+
+  def source_helper(layout_name)
+    if session[:source]
+      greeting = "Thanks for visiting me from #{session[:source]}! You are on #{layout_name} layout"
+      content_tag(:p, greeting, class: "source-greeting")
+    end
   end
 end
