23a6f212d73e3b3cb528217133468ea494a0f415
Iggy <igoririanto@rocketmail.com>
2018-01-18 14:00:48 -0800

    add IggyViewTool Renderer Copyright method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,18 @@ class ApplicationController < ActionController::Base
   include SetSource
   include CurrentUserConcern
   include DefaultPageContent
+
+  before_action :set_copyright
+
+  def set_copyright
+    @copyright = IggyViewTool::Renderer.copyright "Igor Irianto", "All rights reserved"
+  end
+end
+
+module IggyViewTool
+  class Renderer
+    def self.copyright name, msg
+      "&copy; #{Time.now.year} | <b>#{name}</b> #{msg}".html_safe
+    end
+  end
 end
