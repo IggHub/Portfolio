ac789c8b5e3cd664fbb02615c6f9fe7873f91857
Iggy <igoririanto@rocketmail.com>
2018-01-19 12:58:13 -0800

    add iggy_view_tool gem

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,4 +15,8 @@ module ApplicationHelper
       content_tag(:p, greeting, class: "source-greeting")
     end
   end
+
+  def copyright_generator
+    IggyViewTool::Renderer.copyright "Igor Irianto", "All rights reserved"
+  end
 end
